3a137f4012795901db09020e69c07c469619d3d1
charmaine-dlamini-18 <222056401@mycput.ac.za>
2026-08-04 20:39:00 +0200

    Modified DriverNavigator

--- src/navigation/DriverNavigator.tsx
+++ src/navigation/DriverNavigator.tsx
@@ -3,11 +3,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import DriverHomeScreen from '../screens/Driver/DriverHomeScreen';
 import PickupDetailsScreen from '../screens/Driver/PickupDetailsScreen';
+import DeliveryDetailsScreen from '../screens/Driver/DeliveryDetailsScreen';
 
 export type DriverStackParamList = {
   Home: undefined;
 
   OrderDetails: {
+    order: {
+      id: number;
+      orderNumber: string;
+      type: string;
+      customer: string;
+      address: string;
+      time: string;
+      receiver?: string;
+      receiverAddress?: string;
+      phone?: string;
+      laundromat?: string;
+      laundromatAddress?: string;
+      notes?: string;
+    };
+  };
+
+  DeliveryDetails: {
     order: {
       id: number;
       orderNumber: string;
@@ -43,6 +61,11 @@ export default function DriverNavigator() {
       <Stack.Screen
         name="OrderDetails"
         component={PickupDetailsScreen}
+      />
+
+      <Stack.Screen
+        name="DeliveryDetails"
+        component={DeliveryDetailsScreen}
       />
     </Stack.Navigator>
   );
