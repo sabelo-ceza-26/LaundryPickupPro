bfee10aef9bc05b2c87d2a8ec09d6812e9776f6c
sabelo-ceza-26 <220094489@mycput.ac.za>
2026-08-05 21:17:01 +0200

    Customer Navigator modified

--- src/navigation/CustomerNavigator.tsx
+++ src/navigation/CustomerNavigator.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CustomerTabs from './CustomerTabs';
-
-export type CustomerStackParamList = {
-  Main: undefined;
-};
+import NotificationsScreen from '../screens/Customer/NotificationsScreens';
+import OrderDetailsScreen from '../screens/Customer/OrderDetailsScreen';
+import AdressesScreen from '../screens/Customer/AdressesScreen';
+import SettingsScreen from '../screens/Customer/SettingsScreen';
+import SupportScreen from '../screens/Customer/SupportScreen';
+import { OrdersProvider } from '../context/OrdersContext';
+import type { CustomerStackParamList } from './types';
 
 const Stack = createNativeStackNavigator<CustomerStackParamList>();
 
 export default function CustomerNavigator() {
   return (
-    <Stack.Navigator
-      initialRouteName="Main"
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="Main" component={CustomerTabs} />
-    </Stack.Navigator>
+    <OrdersProvider>
+      <Stack.Navigator
+        initialRouteName="Main"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Main" component={CustomerTabs} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        <Stack.Screen name="OrderDetails" component={OrderDetailsScreen} />
+        <Stack.Screen name="Addresses" component={AdressesScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Support" component={SupportScreen} />
+      </Stack.Navigator>
+    </OrdersProvider>
   );
 }
